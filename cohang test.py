a73c70044475a75bd36f7ae0c70ed0dd53a8ac30
Lachlan Carlton <lachlan_carlton@outlook.com>
2025-05-21 17:25:06 +1000

    Added visual art to hangman

--- cohang test.py
+++ cohang test.py
@@ -15,12 +15,14 @@ correctguesses=set()
 incorrectguesses=[]
 maxguesses=len(word)+3
 
-display = ("".join("_" if char != " " else " " for char in word)) 
-print(display)
+
+
+
 
 
 #Makes sure your allowed to guess again and checks if guess is valid
 while len(incorrectguesses) < maxguesses:
+    hangman=['\n', ' ___\n', '|   \n|   \n|   \n|   \n|   \n|\n|___', ' ___\n|   \n|   \n|   \n|   \n|\n|___\n', ' ___\n|  |\n|   \n|   \n|   \n|\n|___\n', ' ___\n|  |\n|  O\n|   \n|   \n|\n|___\n', ' ___\n|  |\n|  O\n|  |\n|   \n|\n|___\n', ' ___\n|  |\n|  O\n|  |\n|  A\n|\n|___\n', ' ___\n|  |\n|  o\n|  +\n|  A\n|\n|___\n']     
     guess = ""
     while len(guess) != 1:
         guess = input("What letter would you like to guess? ").lower()    
@@ -45,12 +47,20 @@ while len(incorrectguesses) < maxguesses:
     #replaces unguessed characters with underscores
     print("Incorrect guesses:", incorrectguesses)
     print("Correct guesses:", correctguesses)
-    print(current_display)
+    print(f"The word is {current_display}")
+    print(hangman[len(incorrectguesses)])
+    #Losing message and shows the Answer
+    if len(incorrectguesses) == len(hangman) - 1:
+        print(F"You have run out of guesses. HANGMAN! The word was: {word}")
+        break
+
+    
+
 
     #Win condition
     if "_" not in current_display:
         print("Congratulations! You've guessed the word correctly!")
+        break
         
         
-#Losing message and shows the Answer
-print(F"You have run out of guesses. HANGMAN! The word was: {word}")
+
